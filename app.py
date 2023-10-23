a7f5dccc7c39bc6f3e3c5dd75e7c9e92ee141075
hiraguchi <shimeshimejiji0@gmail.com>
2023-10-24 02:09:39 +0900

    add upload feature

--- app.py
+++ app.py
@@ -1,42 +1,27 @@
 from flask import Flask
-# from flask import request
-# from flask import render_template
-# import os
+from flask import request
+from flask import render_template
+from flask import redirect
+import os
 
-app = Flask(__name__)
+FILE_PATH = ""
 
-@app.route('/')
-def hello_world():
-    return "<p>Hello world</p>"
+app = Flask(__name__, static_folder='/tmp/')
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     html = '''<!DOCTYPE html>
-# <html lang="ja">
-#     <head>
-#         <title>Windows BMP File Processing</title>
-#         <meta charset="utf-8">
-#     </head>
-#     <body>
-#         <h1>Windows BMP File Processing</h1>
-#         <form method="POST" enctype="multipart/form-data">
-#             <input type="file" name="file">
-#             <input type="submit" value="アップロード">
-#         </form>
-#         <h2>processing button<h2>
-#         <ul>
-#             <li><button>binarize</button></li>
-#         </ul>
-#         <p><button>download BMP File</p>
-#     </body>
-# </hmtl>'''
-    # if request.method == 'POST':
-    #     file_posted = request.files['file']
-    #     file_posted.save(os.path.join('./img/', file_posted.filename))
-    #     return f"{file_posted.filename}がアップロードされました。"
-    #     # return "upload sucessfully"
-    # else:
-    #     return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
+def home():
+    global FILE_PATH 
+    if request.method == 'POST':
+        file_posted = request.files['file']
+        FILE_PATH = os.path.join('/tmp/', file_posted.filename)
+        file_posted.save(FILE_PATH)
+        return redirect('/process')
+    return render_template('upload.html')
+
+@app.route('/process')
+def process():
+    return render_template('process.html', filepath=FILE_PATH)
 
 if __name__ == '__main__':
     app.run()
