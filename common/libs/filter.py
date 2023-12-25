9659b9590b9e7511ef902c3fdf42d38cf95b4e5a
hiraguchi <shimeshimejiji0@gmail.com>
2023-12-25 17:38:24 +0900

    add: Update filter convolution functions and add test code

--- common/libs/filter.py
+++ common/libs/filter.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import List
 from typing import Tuple
 import sys
 
@@ -56,31 +57,37 @@ def get_filter(name_filter: str)->Tuple[int, defaultdict]:
         return 1, None
     return 0, FILTERS[name_filter]
 
-def apply_filter(img: list, img_next: list, 
-                 bit_per_pixcel: int, height: int, 
-                 width: int, filter: list, 
-                 filter_dim: int, apply_dim: int)->Tuple[int, list, int, int]:
-    filter_height = len(filter[0])
-    filter_width =  len(filter[0][0])
+def apply_filter(img: List, bit_per_pixcel: int, height: int,
+                 width: int, filter: List, apply_dims: List[int]) -> Tuple[List, int, int]:
+    # assert len(filter) == 2, f"len(filter) must be 2.\nlen(filter) : {len(filter)}"
+    filter_height = len(filter)
+    filter_width =  len(filter[0])
+    assert filter_height % 2 == 1, f"filter_height must be odd number.\nfilter_height : {filter_width}"
+    assert filter_width % 2 == 1, f"filter_width must be odd number.\nfilter_width : {filter_width}"
+    assert height >= filter_height, f" The height of the image must be greater than or equal to the height of the filter.\nfilter_height : {filter_height}\nheight : {height}"
+    assert width >= filter_width, f" The width of the image must be greater than or equal to the width of the filter.\nfilter_width : {filter_width}\nwidth : {width}"
+
+    img_applied = []
     for y in range(filter_height//2, height-filter_height//2):
         l = []
         for x in range(filter_width//2, width-(filter_width//2)):
             pixcel = []
-            n_value = 0
-            for j in range(-(filter_height//2), (filter_height//2)+1):
-                for i in range(-(filter_width//2), (filter_width//2)+1):
-                    n_value += img[y+j][x+i][apply_dim] * filter[filter_dim][j+(filter_height//2)][i+(filter_width//2)]
-                    
-            if n_value < 0:
+            for color_dim in range(bit_per_pixcel//8):
+                if color_dim not in apply_dims:
+                    pixcel.append(img[y][x][color_dim])
+                    continue
                 n_value = 0
-            if n_value > 255:
-                n_value = 255
+                for j in range(-(filter_height//2), (filter_height//2)+1):
+                    for i in range(-(filter_width//2), (filter_width//2)+1):
+                        n_value += img[y+j][x+i][color_dim] * filter[j+(filter_height//2)][i+(filter_width//2)]
             
-            for i in range(bit_per_pixcel//8):
-                if i == apply_dim:
-                    pixcel.append(n_value)
-                else:
-                    pixcel.append(img[y][x][i])      
+                if n_value < 0:
+                    n_value = 0
+                if n_value > 255:
+                    n_value = 255
+                pixcel.append(n_value)
             l.append(pixcel) 
-        img_next.append(l)
-    return 0, img_next, height-2*(filter_height//2), width-2*(filter_width//2)
+        img_applied.append(l)
+        height_next = height-2*(filter_height//2)
+        width_next = width-2*(filter_width//2)
+    return img_applied, height_next, width_next
